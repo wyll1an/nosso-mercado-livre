0d32413ddaffe5f79a40ddcd667dd7e714c7ec45
willsantos0 <tEMPEST7>
2020-04-28 10:55:46 -0300

    Removendo arquivos

--- src/NossoMercadoLivreAPI.Domain/Interfaces/Repositories/IUserRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using NossoMercadoLivreAPI.Domain.Entities;
-
-namespace NossoMercadoLivreAPI.Domain.Interfaces.Repositories
-{
-    public interface IUserRepository : IRepository<UserEntity>
-    {
-    }
-}
